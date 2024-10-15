70ebb891346528df2d2c215a363273ea80c9a0f4
Maxime Moreillon <m.moreillon@gmail.com>
2024-10-15 17:09:19 +0900

    adapted to new timescaledb backend

--- registration.ts
+++ registration.ts
@@ -2,7 +2,7 @@ const axios = require("axios")
 const dotenv = require("dotenv")
 dotenv.config()
 
-const { FINANCES_API_URL, FINANCES_API_TOKEN, FINANCES_API_ACCOUNT_NAME } =
+const { FINANCES_API_URL, FINANCES_API_TOKEN, FINANCES_API_ACCOUNT_ID } =
   process.env
 
 const options = {
@@ -13,16 +13,17 @@ const options = {
   timeout: 3000,
 }
 
-export const register_transactions = (transactions: any) => {
-  const url = `${FINANCES_API_URL}/accounts/${FINANCES_API_ACCOUNT_NAME}/transactions`
-  // Note: Account is written in the transactions
-  const body = { transactions }
+export const register_transactions = async (transactions: any) => {
+  const url = `${FINANCES_API_URL}/accounts/${FINANCES_API_ACCOUNT_ID}/transactions`
 
-  return axios.post(url, body, options)
+  for (const { date, description, amount } of transactions) {
+    const body = { time: date, description, amount }
+    await axios.post(url, body, options)
+  }
 }
 
 export const register_balance = (balance: number) => {
-  const url = `${FINANCES_API_URL}/accounts/${FINANCES_API_ACCOUNT_NAME}/balance`
+  const url = `${FINANCES_API_URL}/accounts/${FINANCES_API_ACCOUNT_ID}/balance`
 
   const body = {
     balance,
@@ -33,4 +34,4 @@ export const register_balance = (balance: number) => {
 }
 
 export const finance_api_url = FINANCES_API_URL
-export const finance_api_account = FINANCES_API_ACCOUNT_NAME
+export const finance_api_account = FINANCES_API_ACCOUNT_ID
