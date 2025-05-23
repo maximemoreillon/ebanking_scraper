157ab38fe3bf66a4329dc58885ac09689529b31e
Maxime Moreillon <m.moreillon@gmail.com>
2025-05-24 08:00:49 +0900

    fixed icomplete ts conversion

--- registration.ts
+++ registration.ts
@@ -1,9 +1,7 @@
-const axios = require("axios")
-const dotenv = require("dotenv")
-dotenv.config()
+import axios from "axios";
 
-const { FINANCES_API_URL, FINANCES_API_TOKEN, FINANCES_API_ACCOUNT_ID } =
-  process.env
+export const { FINANCES_API_URL, FINANCES_API_TOKEN, FINANCES_API_ACCOUNT_ID } =
+  process.env;
 
 const options = {
   headers: {
@@ -11,27 +9,24 @@ const options = {
     Authorization: `Bearer ${FINANCES_API_TOKEN}`,
   },
   timeout: 3000,
-}
+};
 
 export const register_transactions = async (transactions: any) => {
-  const url = `${FINANCES_API_URL}/accounts/${FINANCES_API_ACCOUNT_ID}/transactions`
+  const url = `${FINANCES_API_URL}/accounts/${FINANCES_API_ACCOUNT_ID}/transactions`;
 
   for (const { date, description, amount } of transactions) {
-    const body = { time: date, description, amount }
-    await axios.post(url, body, options)
+    const body = { time: date, description, amount };
+    await axios.post(url, body, options);
   }
-}
+};
 
 export const register_balance = (balance: number) => {
-  const url = `${FINANCES_API_URL}/accounts/${FINANCES_API_ACCOUNT_ID}/balance`
+  const url = `${FINANCES_API_URL}/accounts/${FINANCES_API_ACCOUNT_ID}/balance`;
 
   const body = {
     balance,
     currency: "JPY",
-  }
+  };
 
-  return axios.post(url, body, options)
-}
-
-export const finance_api_url = FINANCES_API_URL
-export const finance_api_account = FINANCES_API_ACCOUNT_ID
+  return axios.post(url, body, options);
+};
